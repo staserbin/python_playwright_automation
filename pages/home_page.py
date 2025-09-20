66d27aeb315f8e016aafec4dfc7e121515a61dea
Stan_Git <stan@gmail.com>
2025-09-20 06:02:30 -0400

    Code refactoring. Added Allure steps and descriptions

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page, expect
 from utils.text_utils import clean_text
 
@@ -8,18 +9,23 @@ class HomePage:
         self.url = "https://automationexercise.com/"
         self.top_menu = page.locator("ul.nav.navbar-nav > li > a")
 
+    @allure.step("Navigate to the page URL")
     def goto(self):
         self.page.goto(self.url)
         return self
 
+    @allure.step("Check if 'Home' menu item is selected (highlighted in orange)")
     def is_home_selected(self):
         return self.page.get_by_role("link", name="Home").get_attribute("style") == "color: orange;"
 
+    @allure.step("Verify that the carousel is visible on the page")
     def is_carousel_visible(self):
         expect(self.page.locator(".carousel-inner").first).to_be_visible()
 
+    @allure.step("Check that the Features Items title is present")
     def has_features_title(self):
         expect(self.page.locator("div.features_items h2[class*='title']")).to_contain_text("Features Items")
 
-    def get_top_menu_options(self) -> list[str]:
+    @allure.step("Get list of top menu items")
+    def get_menu_items(self) -> list[str]:
         return [clean_text(item.inner_text()) for item in self.top_menu.all()]
--- tests/ui/test_home_page.py
+++ tests/ui/test_home_page.py
@@ -1,20 +1,29 @@
+import allure
+
+
 class TestHomePage:
+
+    @allure.title("Verify homepage loads on website launch")
+    @allure.description("Validate that the main page (homepage) is the first page displayed when accessing the website")
     def test_home_page_is_opened(self, home_page):
-        # Ensure that the 'Home' option on the top bar is orange
-        assert home_page.is_home_selected()
+        with allure.step("Ensure that the 'Home' option on the top bar is orange"):
+            assert home_page.is_home_selected()
 
-        # Verify that the carousel banner is displayed
-        home_page.is_carousel_visible()
+        with allure.step("Verify that the carousel banner is displayed"):
+            home_page.is_carousel_visible()
 
-        # Verify that the 'Features Items' title is displayed
-        home_page.has_features_title()
+        with allure.step("Verify that the 'Features Items' title is displayed"):
+            home_page.has_features_title()
 
-    def test_top_bar_options(self, home_page):
+    @allure.title("Menu items validation")
+    @allure.description("Validate that the menu of the main page contains all expected navigation items")
+    def test_menu_items(self, home_page):
         expected_options = [
             "Home", "Products", "Cart", "Signup / Login", "Test Cases", "API Testing", "Video Tutorials", "Contact us"
         ]
 
-        actual_options = home_page.get_top_menu_options()
+        actual_options = home_page.get_menu_items()
 
-        assert expected_options == actual_options, \
-            f"Expected top bar titles: {expected_options}, but got: {actual_options}"
+        with allure.step("Verify that all expected menu items are displayed"):
+            assert expected_options == actual_options, \
+                f"Expected menu items: {expected_options}, but got: {actual_options}"
